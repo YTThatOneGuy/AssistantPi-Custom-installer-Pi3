6a15da3eb32eb706b1109043dcf50f058bc9772b
YTThatOneGuy <emailthatoneguynow@gmail.com>
2017-09-17 15:46:16 +1000

    Create Install1.sh

---
+++ Install1.sh
@@ -0,0 +1,6 @@
+sudo apt-get update
+sudo apt-get install python3-dev python3-venv # Use python3.4-venv if the package cannot be found.
+python3 -m venv env
+env/bin/python -m pip install --upgrade pip setuptools
+source env/bin/activate
+python -m pip install --upgrade google-assistant-library
